aed3b16a709fbd90c6342b866ff751fbe170103d
arcamp0130 <arcmp0130@gmail.com>
2025-09-20 01:55:36 -0600

    feat: creating object 'problem' to implement a function to actually solve maze

--- public/js/managers/input.manager.js
+++ public/js/managers/input.manager.js
@@ -58,18 +58,46 @@ class InputManager {
         this.#appendListenerToMaze();
     }
 
+    #getMazeMatrix() {
+        // Get cells and initialize maze matrix filled with 'false'
+        const cells = this.maze.querySelectorAll(".cell");
+        const matrix = Array(10).fill(null).map(() => Array(10).fill(false));
+        
+        cells.forEach(cell => {
+            // Get cell coordinates
+            const x = parseInt(cell.dataset.x);
+            const y = parseInt(cell.dataset.y);
+
+            // A cell is considered "free" if it isn't wall, target or source point
+            matrix[y][x] = cell.dataset.cellType === this.cellType.free;
+        });
+        
+        return matrix;
+    }
+
     async #solveMaze() {
         // TODO
         // Call function to solve algorithm with selected algorithm and maze as arguments.
         // Use 'Problem' function to define a new problem, state and several other implmementations.
-        const algorithm = document.querySelector("input[name='algorithm']:checked").value;
+        const problem = {
+            algorithm: document.querySelector("input[name='algorithm']:checked").value,
+            maze: this.#getMazeMatrix(),
+            startCell: {
+                x: this.startCell.dataset.x,
+                y: this.startCell.dataset.y
+            },
+            targetCell: {
+                x: this.targetCell.dataset.x,
+                y: this.targetCell.dataset.y
+            }
+        }
         
         // Disabling buttons 'start' and 'reset'
         this.solveBtn.disabled = true;
         this.resetBtn.disabled = true;
 
         // Using this kind of implementation only to show that webpage is ready to await for response
-        console.log(algorithm);
+        console.log(problem);
         await setTimeout(() => console.log("One second passed"), 1000);
         
         // Enabling buttons 'start' and 'reset' when getting an answer from algorithm
