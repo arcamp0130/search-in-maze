c98bedf7dc2564606749ceba1b6337c09185b876
arcamp0130 <arcmp0130@gmail.com>
2025-09-20 21:03:15 -0600

    feat: Filling Stack functions for future implementation.

--- public/js/data-structures/stack.structure.js
+++ public/js/data-structures/stack.structure.js
@@ -1,23 +1,33 @@
+// LIFO
+// Last In - First Out
 class Stack {
     constructor() {
         this.items = [];
     }
-    
-    pop() {
 
+    pop() {
+        return this.isEmpty()
+            ? null // null if empty
+            : this.items.pop();
     }
 
     push(item) {
-
+        this.items.push(item);
     }
 
     peek() {
+        return this.isEmpty()
+            ? null // null if empty
+            : this.items[this.size() - 1];
+    }
 
+    size() {
+        return this.items.length;
     }
 
     isEmpty() {
-
+        return this.items.length === 0;
     }
- }
+}
 
 export default Stack;
