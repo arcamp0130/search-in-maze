be1fb612c576e409e947f95a1ba9339014208747
arcamp0130 <arcmp0130@gmail.com>
2025-09-20 21:06:56 -0600

    refactor: Changing export statement and adding comments to stack methods

--- public/js/data-structures/stack.structure.js
+++ public/js/data-structures/stack.structure.js
@@ -1,33 +1,36 @@
 // LIFO
 // Last In - First Out
-class Stack {
+export default class Stack {
     constructor() {
         this.items = [];
     }
 
+    // Remove and return last element in array
     pop() {
         return this.isEmpty()
             ? null // null if empty
             : this.items.pop();
     }
 
+    // Add a new element as the last one in array
     push(item) {
         this.items.push(item);
     }
 
+    // Only return last element in array
     peek() {
         return this.isEmpty()
             ? null // null if empty
             : this.items[this.size() - 1];
     }
 
+    // Return array length
     size() {
         return this.items.length;
     }
 
+    // Check if array is empty and return confirmation
     isEmpty() {
         return this.items.length === 0;
     }
 }
-
-export default Stack;
