b783dc7fedd87f4bc764e61acdebb44da8a258b5
arcamp0130 <arcmp0130@gmail.com>
2025-09-20 20:51:59 -0600

    feat: Creating Queue and Stack classes with empty main functions.

---
+++ public/js/data-structures/queue.structure.js
@@ -0,0 +1,24 @@
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+    
+    pop() {
+
+    }
+
+    push(item) {
+
+    }
+
+    peek() {
+
+    }
+
+    isEmpty() {
+
+    }
+
+}
+
+export default Queue;
---
+++ public/js/data-structures/stack.structure.js
@@ -0,0 +1,23 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    
+    pop() {
+
+    }
+
+    push(item) {
+
+    }
+
+    peek() {
+
+    }
+
+    isEmpty() {
+
+    }
+ }
+
+export default Stack;
